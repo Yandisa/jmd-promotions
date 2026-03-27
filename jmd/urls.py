7471c4a25b7d6abd53e754d3da96319c938d70e1
Yandisa <gaju.yandisa@gmail.com>
2026-03-27 23:17:43 +0200

    Update urls.py

--- jmd/urls.py
+++ jmd/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header = "JMD Promotions Admin"
-admin.site.site_title = "JMD Admin"
+admin.site.site_title  = "JMD Admin"
 admin.site.index_title = "Welcome to JMD Promotions Dashboard"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('shop/', include('store.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve media files in both DEBUG and production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]MEDIA_ROOT)
